1bff627faa0ea6f6d24b309db4b93dc240bdedcd
IsabelaAT <60246348+IsabelaAT@users.noreply.github.com>
2021-09-25 12:25:53 -0500

    Yellowball (#3)

--- Assets/Scripts/yellow.cs
+++ Assets/Scripts/yellow.cs
@@ -2,30 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Blue : MonoBehaviour, Iinteractable {
-
+public class yellow : MonoBehaviour, Iinteractable
+{
     Renderer playerRenderer;
     Renderer myRenderer;
     PlayerMovement speedPlayer;
-    float newSpeed = 4f;
-    
-   
-   
-    public void Interact(Player player) {
+    float newSpeed = 15f;
+    private Vector3 scaleChange;
+
+    public void Interact(Player player)
+    {
         ChangeColor(player);
         ChangeSpeed(player);
-        
+        ChangeScale(player);
     }
 
-    void ChangeColor(Player player) {
+    void ChangeColor(Player player)
+    {
         playerRenderer = player.GetComponentInChildren<Renderer>();
         myRenderer = GetComponent<Renderer>();
         playerRenderer.material.color = myRenderer.material.color;
     }
 
-    void ChangeSpeed(Player player) {
+    void ChangeSpeed(Player player)
+    {
         speedPlayer = player.GetComponent<PlayerMovement>();
         speedPlayer.magnitud = newSpeed;
     }
-    
+    void ChangeScale(Player player)
+    {
+        scaleChange = new Vector3(2f, 2f, 2f);
+        player.transform.localScale = scaleChange;
+    }
 }
